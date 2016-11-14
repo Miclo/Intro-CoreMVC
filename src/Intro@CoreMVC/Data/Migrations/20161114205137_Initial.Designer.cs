baa730a09605462988585c4a7c1ef41f9ce22691
Lorenz Michels <Lorenz.Michels@gmail.com>
2016-11-14 23:14:37 +0100

    https://mva.microsoft.com/en-US/training-courses/introduction-to-asp-net-core-1-0-16841?l=eYlqdeE6C_1006218965

--- src/Intro@CoreMVC/Data/Migrations/20161114205137_Initial.Designer.cs
+++ src/Intro@CoreMVC/Data/Migrations/20161114205137_Initial.Designer.cs
@@ -8,9 +8,10 @@ using Intro_CoreMVC.Data;
 namespace Intro_CoreMVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161114205137_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
---
+++ src/Intro@CoreMVC/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intro_CoreMVC.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
